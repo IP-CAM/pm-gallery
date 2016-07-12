2340cfa18864ba7289ec7a48a3f183183a1aca29
Pekka Mansikka <pekka2@users.noreply.github.com>
2016-07-12 15:11:39 +0300

    Add files via upload

---
+++ upload/catalog/language/english/module/pm_gallery.php
@@ -0,0 +1,36 @@
+<?php
+
+// Text
+$_['heading_title']       = 'Photo Gallery';
+$_['text_description']    = 'Own Photo Albums';
+$_['text_keywords']       = 'photo,image,photos';
+$_['text_home']           = 'Home';
+
+$_['error_name']          = 'Name is required!';
+$_['error_comment']       = 'Comment text is required!';
+// txt
+$_['txt_comm_auto_string'] = 'Pm Image Gallery - Picture %x of %X Filename: %f, Gallery: %g';
+$_['txt_nav_next']         = 'Next page';
+$_['txt_nav_back']         = 'Previous page';
+$_['txt_nav_maxi']         = 'Gallery to maxim size'; 
+$_['txt_max']              = 'Maxim size';
+$-['txt_nav_next']         = 'Next picture';
+$_['txt_nav_prev']         = 'Previous picture';
+$_['txt_nav_close']        = 'Close'; 
+$_['txt_nav_gps_coord']    = 'Show location on map';
+$_['txt_nav_vcomm']        = 'Add/See viewer comments';
+$_['txt_nav_download']     = 'Download full resolution picture';
+$_['txt_slideshow_start']  = 'Start slideshow';
+$_['txt_slideshow']        = 'Slideshow';
+$_['txt_slideshow_stop']   = 'Stop slideshow'; 
+$_['txt_help_text']        = 'Move your mouse [mouse] beyond the sides of the image to see what image comes next/was last in the gallery. If you move your mouse even further to the border of the window you can see the next/last couple of images. Move your mouse up to the top to view navigation controls and move it down to see a link address of the picture displayed.', 
+$_['txt_vcomm_lac']        = 'Leave a comment';
+$_['txt_vcomm_name']       = 'Name';
+$_['txt_vcomm_comm']       = 'Comment';
+$_['txt_vcomm_post']       = 'Post';
+$_['txt_vcomm_clk']        = 'Click on the image to flip back to the full image.';
+$_['txt_vcomm_ncy']        = 'No comments yet.';
+$_['txt_vcomm_title']      = 'Comments';
+$_['txt_new_comment']      = 'A new comment has been made for one of your images. Visit <a href="%s">%s2</a> and flip the picture to read all comments for this image.\\nThis is the last comment. This comment is moderated and is not readable in public yet. You have the option publish this comment when you login as admin.';
+
+?>
