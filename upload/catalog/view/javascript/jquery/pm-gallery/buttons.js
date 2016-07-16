7ea6c930608bbcd3de5d8a57cca1d29d9770ed5c
Pekka Mansikka <pekka2@users.noreply.github.com>
2016-07-16 16:11:35 +0300

    Update buttons.js

--- upload/catalog/view/javascript/jquery/pm-gallery/buttons.js
+++ upload/catalog/view/javascript/jquery/pm-gallery/buttons.js
@@ -1,16 +1,18 @@
  function lourButtons(folder_id,frame_id,frame_height,pm_th_title,txt_nav_maxi,txt_max){
     var height = parseInt(frame_height)+80;
+    var bheight = height-46;
     $('#'+frame_id).append('<div id="test_' + frame_id + '_hide_tb">');
     $('#'+frame_id).css({"height": + height + "px"});
-    $('#test_'+frame_id + '_hide_tb').css({"position":"absolute"}); 
-    $('#test_'+frame_id + '_hide_tb').css({"z-index":"100000"});   
-    $('#test_'+frame_id + '_hide_tb').css({"width":"98%"});
-    $('#test_'+frame_id + '_hide_tb').css({"height":"45px"});
+    $('#test_'+frame_id + '_hide_tb').css({"position":"relative"}); 
+    $('#test_'+frame_id + '_hide_tb').css({"float":"right"});
+    $('#test_'+frame_id + '_hide_tb').css({"z-index":"100"});   
+    $('#test_'+frame_id + '_hide_tb').css({"width":"37px"});
+    $('#test_'+frame_id + '_hide_tb').css({"height":"37px"});
     $('#test_'+frame_id + '_hide_tb').css({"text-align":"right"});
-    $('#test_'+frame_id + '_hide_tb').css({"margin-top":"200px"});
-    $('#test_'+frame_id + '_hide_tb').css({"background":"red none repeat scroll 0% 0%"});
+    $('#test_'+frame_id + '_hide_tb').css({"margin-top":bheight + "px"});
+    $('#test_'+frame_id + '_hide_tb').css({"background":"#ffffff none repeat scroll 0% 0%"});
     $('#test_'+frame_id + '_hide_tb').css({"border":"2px solid rgb(0, 0, 0)"});
-    $('#test_'+frame_id + '_hide_tb').css({"padding":"1px 3px 3px"});
+    $('#test_'+frame_id + '_hide_tb').css({"padding":"3px"});
     $('#test_'+frame_id + '_hide_tb').show(); 
 
     $('#test_'+frame_id + '_hide_tb').append('<img id="pm_nav_full" src="catalog/view/javascript/jquery/pm-gallery/image/pm_nav_full.png" onClick="startExplorer(\'' + folder_id + '\',\'' + frame_id + '\',\'' + pm_th_title + '\');" title="'+ txt_nav_maxi + '" alt="' + txt_max + '">'); 
